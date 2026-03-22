7fdc0b1f6cf153df796ba6ba42c78aa4d61e0180
Arthur Medeiros <arthurmnpro7@gmail.com>
2026-03-22 11:45:53 -0300

    docs: organização dos arquivos

---
+++ include/lista_compra.h
@@ -0,0 +1,19 @@
+#include <vector>
+#include <map>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+typedef struct {
+    int data;
+    char codeCliente[9];
+    int codeProduto;
+    char produto[50];
+} Clientes;
+
+void gerarListaCompras(
+    vector<string>& codeClientes,map<string,int>& clienteIndex,
+    vector<string>& codeProdutos, map<int, int>& produtoIndex,
+    vector<string>& listaCompras);
+
