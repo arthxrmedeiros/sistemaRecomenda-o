3b25c2f863e7ab86917512b568bb820b0e20143c
Arthur Medeiros <arthurmnpro7@gmail.com>
2026-03-24 04:20:17 -0300

    docs: adição dos arquivos na main

---
+++ src/lista_compra.cpp
@@ -0,0 +1,45 @@
+#include "lista_compra.h"
+#include <stdio.h>
+#include <string.h>
+
+void gerarListaCompras(ListaCompra &lc) {
+
+    FILE *arquivo = fopen("dados_venda_cluster_0.csv", "r");
+
+    int data;
+    char codigoCliente[9];
+    int codigoProduto;
+    char nomeProduto[50];
+
+    fscanf(arquivo, "%*[^\n]\n");
+
+    while (fscanf(arquivo, "%d,%8[^,],%d,%49[^\n]\n",
+                  &data, codigoCliente, &codigoProduto, nomeProduto) == 4) {
+
+        std::string clienteStr = codigoCliente;
+        std::string produtoStr = nomeProduto;
+
+        if (lc.indice_cliente.find(clienteStr) == lc.indice_cliente.end()) {
+            int novoIndice = lc.clientes.size();
+
+            lc.clientes.push_back(clienteStr);
+            lc.indice_cliente[clienteStr] = novoIndice;
+
+            lc.compras.push_back(std::vector<int>());
+        }
+
+        if (lc.indice_produto.find(produtoStr) == lc.indice_produto.end()) {
+            int novoIndice = lc.produtos.size();
+
+            lc.produtos.push_back(produtoStr);
+            lc.indice_produto[produtoStr] = novoIndice;
+        }
+
+        int idxCliente = lc.indice_cliente[clienteStr];
+        int idxProduto = lc.indice_produto[produtoStr];
+
+        lc.compras[idxCliente].push_back(idxProduto);
+    }
+
+    fclose(arquivo);
+}
---
+++ src/similaridadeNew.cpp
@@ -0,0 +1,110 @@
+#include "similaridade.h"
+
+std::vector<std::vector<int>> gerarMatrizCompras(
+    std::vector<std::vector<int>>& listaCompras,
+    int numClientes,
+    int numProdutos
+) {
+    std::vector<std::vector<int>> matrizCompras(numClientes, std::vector<int>(numProdutos, 0));
+
+    for (int i = 0; i < numClientes; i++) {
+        for (int j = 0; j < listaCompras[i].size(); j++) {
+            int idProduto = listaCompras[i][j];
+            matrizCompras[i][idProduto] = 1;
+        }
+    }
+
+    return matrizCompras;
+}
+
+std::vector<std::vector<int>> gerarMatrizIntersecao(
+    std::vector<std::vector<int>>& matrizCompras,
+    int numClientes,
+    int numProdutos
+) {
+    std::vector<std::vector<int>> matrizIntersecao(numClientes, std::vector<int>(numClientes, 0));
+
+    for (int i = 0; i < numClientes; i++) {
+        for (int j = 0; j < numClientes; j++) {
+            for (int k = 0; k < numProdutos; k++) {
+                matrizIntersecao[i][j] += matrizCompras[i][k] * matrizCompras[j][k];
+            }
+        }
+    }
+
+    return matrizIntersecao;
+    }
+
+    std::vector<std::vector<int>> gerarMatrizIntersecaoOtimizada(
+        std::vector<std::vector<int>>& matrizCompras,
+        int numClientes,
+        int numProdutos
+    ) {
+        std::vector<std::vector<int>> matrizIntersecao(numClientes, std::vector<int>(numClientes, 0));
+
+        for (int i = 0; i < numClientes; i++) {
+            for (int j = i; j < numClientes; j++) { 
+
+                int soma = 0;
+
+                for (int k = 0; k < numProdutos; k++) {
+                    soma += matrizCompras[i][k] * matrizCompras[j][k];
+                }
+
+                matrizIntersecao[i][j] = soma;
+                matrizIntersecao[j][i] = soma; 
+            }
+        }
+
+        return matrizIntersecao;
+    }    
+
+
+std::vector<std::vector<float>> gerarMatrizSimilaridade(
+    std::vector<std::vector<int>>& matrizCompras,
+    int numClientes,
+    int numProdutos,
+    bool otimizado
+) {
+
+    std::vector<std::vector<int>> matrizIntersecao;
+
+    if (otimizado) {
+        matrizIntersecao = gerarMatrizIntersecaoOtimizada(
+            matrizCompras, numClientes, numProdutos
+        );
+    } else {
+        matrizIntersecao = gerarMatrizIntersecao(
+            matrizCompras, numClientes, numProdutos
+        );
+    }
+
+    std::vector<std::vector<float>> matrizSimilaridade(numClientes, std::vector<float>(numClientes, 0.0));
+
+    for (int i = 0; i < numClientes; i++) {
+
+        int totalI = 0;
+        for (int k = 0; k < numProdutos; k++) {
+            totalI += matrizCompras[i][k];
+        }
+
+        for (int j = 0; j < numClientes; j++) {
+
+            int totalJ = 0;
+            for (int k = 0; k < numProdutos; k++) {
+                totalJ += matrizCompras[j][k];
+            }
+
+            int intersecao = matrizIntersecao[i][j];
+            int uniao = totalI + totalJ - intersecao;
+
+            if (uniao != 0) {
+                matrizSimilaridade[i][j] = 1.0 - ((float)intersecao / (float)uniao);
+            } else {
+                matrizSimilaridade[i][j] = 0.0;
+            }
+        }
+    }
+
+    return matrizSimilaridade;
+}
